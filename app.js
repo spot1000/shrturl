23bf3b0ddfbdd11b181ae1df0f5703f8175cc83a
Adam Lichter <adamlichter@gmail.com>
2017-01-16 22:59:54 -0330

    was playing around withm ongodb

--- app.js
+++ app.js
@@ -9,30 +9,28 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'pug');
 
-app.get('/', function(req,res) {
-  res.send('enter an address at the end of the URL to shorten')
-});
 
-app.get('/:url', function(req,res) {
   MongoClient.connect("mongodb://adam:yukonho1984@ds111479.mlab.com:11479/shrturl" , function(err, db) {
     if (err) {
       console.log('unable to connect to the MongoDB server. Error:', err)
     } else {
-
-      console.log('Connection establish to monogDB server');
-      var collection = db.collection('shrturls');
-
-      collection.insert(function(err, docs) {
-        if (err) {
-          throw err;
-        } else {
-          console.log(docs);
-        }
-      } )
+      console.log('connection established');
     }
-  });
-});
 
-app.listen(port, function() {
-  console.log('server established, listening on port: ', port);
-})
+
+    var collection = db.collection('shrturls')
+
+    app.get('/', function(req,res) {
+      res.send('enter in an address to shorten it!')
+    });
+
+    app.get('/:url', function(req,res) {
+      collection.insert({'url':req.params.url});
+      res.send(collection.find({'url':req.params.url}))
+    })
+
+    app.listen(port, function() {
+      console.log('server established, listening on port: ', port);
+    });
+
+  });
